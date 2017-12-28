f61908bd42e9bcd817e694182212cf1f189251e5
batru96 <gietbarca@gmail.com>
2017-12-28 21:52:59 +0700

    Dang kiem tra theo hang

--- app/src/main/java/com/example/batru/gamecaro/fragments/GameFragment.kt
+++ app/src/main/java/com/example/batru/gamecaro/fragments/GameFragment.kt
@@ -1,6 +1,8 @@
 package com.example.batru.gamecaro.fragments
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -22,6 +24,12 @@ class GameFragment : BaseFragment() {
     private val numButtons = 20
     private val numLayouts = 13
 
+    private val idDrawerButtonDefault = R.drawable.button_sign_in
+    private val idDrawerButtonA = R.drawable.button_player_a
+    private val idDrawerButtonB = R.drawable.button_player_b
+    private lateinit var mDrawableA: Drawable
+    private lateinit var mDrawableB: Drawable
+
     private var isPlayerA = true
     private var isYourTurn = true
     private var mRoom = ""
@@ -29,8 +37,38 @@ class GameFragment : BaseFragment() {
     private val labelSendXAndY = "USER_SEND_X_Y_TO_ROOM"
     private val labelOnListenXAndY = "SERVER_SEND_X_Y_TO_ROOM"
 
+    private val onListenXAndY = Emitter.Listener { args ->
+        activity.runOnUiThread {
+            val obj = args[0] as JSONObject
+            val playerA = obj.getBoolean("isPlayerA")
+
+            obj.remove("room")
+            obj.remove("isPlayerA")
+            val button = mRootView.findViewWithTag<Button>(obj.toString())
+            if (button != null) {
+                if (playerA) {
+                    obj.put("playerName", "A")
+                    button.tag = obj.toString()
+                    button.background = mDrawableA
+                } else {
+                    obj.put("playerName", "B")
+                    button.tag = obj.toString()
+                    button.background = mDrawableB
+                }
+                isWinner(button)
+                // Nếu người A gửi và người B nhận, đến lượt người B được gửi
+                if (playerA != isPlayerA) {
+                    isYourTurn = true
+                }
+            }
+        }
+    }
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         mRootView = inflater!!.inflate(R.layout.fragment_game, container, false)
+
+        mDrawableA = activity.getDrawable(R.drawable.button_player_a)
+        mDrawableB = activity.getDrawable(R.drawable.button_player_b)
 
         if (arguments != null) {
             if (!arguments.getBoolean(GameActivity.tagBundlePlayerA)) {
@@ -45,28 +83,6 @@ class GameFragment : BaseFragment() {
         return mRootView
     }
 
-    private val onListenXAndY = Emitter.Listener { args ->
-        activity.runOnUiThread {
-            val obj = args[0] as JSONObject
-            val playerA = obj.getBoolean("isPlayerA")
-
-            obj.remove("room")
-            obj.remove("isPlayerA")
-            val button = mRootView.findViewWithTag<Button>(obj.toString())
-            if (button != null) {
-                if (playerA) {
-                    button.setBackgroundResource(R.drawable.button_player_a)
-                } else {
-                    button.setBackgroundResource(R.drawable.button_player_b)
-                }
-                // Nếu người A gửi và người B nhận, đến lượt người B được gửi
-                if (playerA != isPlayerA) {
-                    isYourTurn = true
-                }
-            }
-        }
-    }
-
     private fun drawLayouts(rootView: View) {
         for (i in 0 until numLayouts) {
             mLinearGame = rootView.findViewById(R.id.linearGame)
@@ -76,6 +92,7 @@ class GameFragment : BaseFragment() {
 
     private fun drawButtons(position: Int): LinearLayout {
         val layout = LinearLayout(activity)
+        layout.tag = position
         layout.orientation = LinearLayout.HORIZONTAL
         layout.layoutParams = LinearLayout.LayoutParams(matchParent, wrapContent, 1.0f)
 
@@ -92,20 +109,47 @@ class GameFragment : BaseFragment() {
             jsonObject.put("room", mRoom)
             jsonObject.put("isPlayerA", isPlayerA)
 
-            button.setBackgroundResource(R.drawable.button_sign_in)
+            button.setBackgroundResource(idDrawerButtonDefault)
             button.layoutParams = params
             layout.addView(button)
 
-            button.setOnClickListener { handleButtonClicked(button, jsonObject) }
+            button.setOnClickListener { handleButtonClicked(jsonObject) }
         }
 
         return layout
     }
 
-    private fun handleButtonClicked(button: Button, obj: JSONObject) {
+    private fun handleButtonClicked(obj: JSONObject) {
         if (isYourTurn) {
             mSocket.emit(labelSendXAndY, obj)
             isYourTurn = false
         }
+    }
+
+    private fun isWinner(button: Button): Boolean {
+        val obj = JSONObject(button.tag as String)
+        val x = obj.getInt("x")
+        val y = obj.getInt("y")
+
+        // region Kiem tra theo hang ngang
+        val layout = mRootView.findViewWithTag<LinearLayout>(y)
+
+        var minX = x - 4
+        var maxX = x + 4
+        if (minX <= 0)
+            minX = 0
+        if (maxX >= numButtons - 1)
+            maxX = numButtons - 1
+
+        val checks = ArrayList<Boolean>()
+        for (i in minX..maxX) {
+            val mButton = layout.getChildAt(i) as Button
+            Log.d(mTagGameFragment, "x: $i -- ${mButton.tag}")
+        }
+
+
+        //endregion
+
+        return false
     }
 }
